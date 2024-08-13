79307627659f845e2d5577d14d6e568200b8774e
satyam <satyamkale271@gmail.com>
2024-08-13 21:16:38 +0530

    state.count - state.step

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,10 +1,20 @@
+import { type } from "@testing-library/user-event/dist/type";
 import { useReducer } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - 1;
-  if (action.type === "setCount") return action.payload;
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    default:
+      throw new Error("unknown action");
+  }
 }
 function DateCounter() {
   // const [count, setCount] = useState(0);
@@ -37,6 +47,7 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
     // setStep(Number(e.target.value));
   };
 
