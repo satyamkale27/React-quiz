ea6073e48f1cd1ae2796914e232e191c7cbd1db6
satyam <satyamkale271@gmail.com>
2024-08-14 17:42:44 +0530

    another component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Header from "./Header";
+import Main from "./main";
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Main className="main">
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
---
+++ src/main.js
@@ -0,0 +1,5 @@
+function main({ children }) {
+  return <main className="main">{children}</main>;
+}
+
+export default main;
