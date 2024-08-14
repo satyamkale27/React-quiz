25f395f69b3db3f446b132ca47f9c59be626ddee
satyam <satyamkale271@gmail.com>
2024-08-14 18:47:06 +0530

    error dispatch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./main";
+import { type } from "@testing-library/user-event/dist/type";
+const initialState = {
+  questions: [],
+
+  // 'loading, error, ready, active, finished //
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unkonwn");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"));
-  });
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="App">
       <Header />
