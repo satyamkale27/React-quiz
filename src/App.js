87eec1f1ac5a98de937f6c22cf920222531b5cc6
satyam <satyamkale271@gmail.com>
2024-08-14 18:09:32 +0530

    useEffect used to fetch fake api data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./main";
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("Error"));
+  });
   return (
     <div className="App">
       <Header />
