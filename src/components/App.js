75fbe44ed15b22292476cf54c8249e1e5b908938
satyam <satyamkale271@gmail.com>
2024-08-15 15:15:43 +0530

    complex state updating log and correct option increases points

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,7 @@ const initialState = {
   status: "loading",
   index: 0,
   answer: null,
+  points: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -29,9 +30,14 @@ function reducer(state, action) {
         status: "active",
       };
     case "newAnswer":
+      const question = state.questions.at(state.index);
       return {
         ...state,
         answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
       };
     default:
       throw new Error("Action unkonwn");
