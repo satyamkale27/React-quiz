16ca1db28168387012127a1e1d33bcdca84b799c
satyam <satyamkale271@gmail.com>
2024-08-15 14:58:14 +0530

    nested turnery for class rendering correct option

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,12 +1,21 @@
 import { type } from "@testing-library/user-event/dist/type";
 
 function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
       {question.options.map((option, index) => (
         <button
-          className="btn btn-option"
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
           key={option}
+          disabled={hasAnswered}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
         >
           {option}
