adef0cc00e5650702ea02f861095bcfcf3e3a9a9
satyam <satyamkale271@gmail.com>
2024-08-17 17:39:42 +0530

    finish quiz

--- src/components/App.js
+++ src/components/App.js
@@ -52,6 +52,8 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
     default:
       throw new Error("Action unkonwn");
   }
@@ -107,6 +109,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
